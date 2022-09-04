7cf7b324460247fecec5142cbbf970729a6f1839
Irfan Muqorib <94688608+Irfanmqrb25@users.noreply.github.com>
2022-09-04 11:49:54 +0700

    Update NoteItem.js

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,13 +4,13 @@ import NoteDeleteButton from './NoteDeleteButton';
 import NoteArchiveButton from './NoteArchiveButton';
 
 
-function NoteItem({ title, body, id, onDelete, onArchieve, isarchieve }) {
+function NoteItem({ title, body, id, onDelete, onArchive, isArchive }) {
     return (
         <div className="note-item">
             <NoteContent title={title} body={body} />
             <div className="note-item__action">
                 <NoteDeleteButton id={id} onDelete={onDelete} />
-                <NoteArchiveButton id={id} onArchive={onArchieve} isArchived={isarchieve} />
+                <NoteArchiveButton id={id} onArchive={onArchive} isArchive={isArchive} />
             </div>
         </div>
     )
