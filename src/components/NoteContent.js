6b7211c5f6200bcf869ae3957b0c4973e752a770
Irfan Muqorib <94688608+Irfanmqrb25@users.noreply.github.com>
2022-09-05 15:47:07 +0700

    fix bug - date

--- src/components/NoteContent.js
+++ src/components/NoteContent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {showFormattedDate} from '../utils/index'
 
-function NoteContent({title, body}) {
+function NoteContent({title, body, date}) {
     return (
         <div className='note-item__content'>
             <h3 className='note-item__title'>{title}</h3>
-            <p className='note-item__date'>{showFormattedDate(new Date())}</p>
+            <p className='note-item__date'>{showFormattedDate(date)}</p>
             <p className='note-item__body'>{body}</p>
         </div>
     )
